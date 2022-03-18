5df53f0770db3ba567c1a495c951555f02b106bd
awilliams <645192+awilliams@users.noreply.github.com>
2022-03-18 13:56:22 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # virtu
-Block Vizio televisions from accessing the internet and creating a WiFi access point
+
+Stop Vizio TV from accessing the internet *and* from starting an open WiFi access point.
+
+### About
+
+"_[Vizio makes nearly as much money from ads and data as it does from TVs](https://www.engadget.com/vizio-q1-earnings-inscape-013937337.html)_".
+
+Vizio televisions can be unplugged from the internet, unfortunately this may trigger the television to stand up an open WiFi access point (`Television.e000`).
+What follows is a (convoluted) setup to block the television from the internet, along with preventing it from creating an AP.
+
+### Connectivity Check
+
+Through experimentation, it was found that the television will *not* start an AP if it can make a HTTPS requests to `connectivitycheck.gstatic.com`.
+
+The following firewall/iptables rule can be used to block all traffic except for TCP traffic on port 443 to the IP range of `connectivitycheck.gstatic.com`.
+The IP range for `connectivitycheck.gstatic.com` (in this case `142.250.0.0/15`) was determined by
+ 1. Doing a DNS lookup of `connectivitycheck.gstatic.com`
+ 2. Then finding the corresponding block in https://www.gstatic.com/ipranges/goog.json
+
+```
+Chain Blackhole (1 references)
+target     prot opt source               destination
+RETURN     tcp  --  0.0.0.0/0            142.250.0.0/15       /* Blackhole-20 */ state NEW,RELATED,ESTABLISHED match-set Blackhole src tcp dpt:443
+DROP       all  --  0.0.0.0/0            0.0.0.0/0            /* Blackhole-30 */ match-set Blackhole src
+RETURN     all  --  0.0.0.0/0            0.0.0.0/0            /* Blackhole-10000 default-action accept */
+```
+
+The match-set "Blackhole" was assigned to the television's IP.
+
+### DNS
+
+As an additional step, the television's DNS can be redirected to the DNS resolver found in this repository.
+The resolver returns the loopback address to all queries except those in a "forward" list (default is `connectivitycheck.gstatic.com`).
+
+Vizio televisions have been found to make a significant number of DNS requests.
+
+#### Usage
+
+Build:
+```shell
+go build
+```
+
+Run:
+```shell
+sudo ./virtu -port 53
+```
+
+#### dnsmasq
+
+The DNS settings of the television can be set manually.
+
+If using `dnsmasq` as a DHCP server, the following configuration can be used to assign the television a separate DNS resolver (such as `virtu`).
+
+```
+# Set hosts tagged with 'blackhole-dns' to use 10.0.1.8 as their DNS server.
+dhcp-option=tag:blackhole-dns,option:dns-server,10.0.1.8
+```
+
+Then "tag" hosts with `blackhole-dns` to assign them the alternative DNS.
+```
+# tv
+dhcp-host=aa:bb:cc:dd:ee:ff,set:blackhole-dns,10.0.1.110,tv
+```
---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/miekg/dns"
+)
+
+const defaultTTL = 3600
+
+func main() {
+	port := flag.Int("port", 1053, "DNS server listening port")
+	ip4 := flag.String("ip4", "127.0.0.1", "Default A record response IP")
+	ip6 := flag.String("ip6", "::1", "Default AAAA record response IP")
+	upstream := flag.String("upstream", "8.8.8.8", "Upstream DNS resolver")
+	forward := flag.String("forward", "connectivitycheck.gstatic.com,pool.ntp.org", "A record names to forward to upstream DNS (separate multiple values by comma)")
+	logDate := flag.Bool("log-date", true, "include date/time in log output")
+	flag.Parse()
+
+	if !*logDate {
+		log.SetFlags(0)
+	}
+
+	if *upstream == "" {
+		log.Fatal("-upstream cannot be blank")
+	}
+	respA := net.ParseIP(*ip4)
+	respAAAA := net.ParseIP(*ip6)
+	if len(respA) == 0 {
+		log.Fatalf("invalid IPv4 %q", *ip4)
+	}
+	if len(respAAAA) == 0 {
+		log.Fatalf("invalid IPv6 %q", *ip4)
+	}
+
+	forwardNames := strings.Split(*forward, ",")
+	for i, n := range forwardNames {
+		l := len(n)
+		if l == 0 || n[l-1] == '.' {
+			continue
+		}
+		forwardNames[i] = n + "."
+	}
+
+	h := dnsHandler{
+		respA:    respA,
+		respAAAA: respAAAA,
+		upstream: *upstream,
+		forward:  forwardNames,
+	}
+
+	dsrv := dns.Server{
+		Addr:    fmt.Sprintf(":%d", *port),
+		Net:     "udp",
+		Handler: &h,
+	}
+	go func() {
+		if err := dsrv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	<-stop
+	dsrv.Shutdown()
+}
+
+type dnsHandler struct {
+	respA    net.IP
+	respAAAA net.IP
+	upstream string
+	forward  []string
+}
+
+func (d *dnsHandler) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
+	var q dns.Question
+	if len(req.Question) > 0 {
+		q = req.Question[0]
+	}
+
+	var (
+		respIP  net.IP
+		respTTL uint32
+	)
+	defer func() {
+		var ip string
+		if len(respIP) == 0 {
+			ip = "(Refused)"
+		} else {
+			ip = respIP.String()
+		}
+		log.Printf("DNS: % -4s %s > %s (%d)", dns.Type(q.Qtype), q.Name, ip, respTTL)
+	}()
+
+	var resp dns.Msg
+	resp.Authoritative = true
+	resp.SetReply(req)
+
+	shouldForward := func(name string) bool {
+		for _, n := range d.forward {
+			if n == name {
+				return true
+			}
+		}
+		return false
+	}
+
+	switch q.Qtype {
+
+	case dns.TypeAAAA:
+		if shouldForward(q.Name) {
+			in, err := dns.Exchange(req, fmt.Sprintf("%s:53", d.upstream))
+			if err != nil {
+				log.Printf("Error requesting from upstream DNS: %s", err)
+			} else {
+				if a, ok := in.Answer[0].(*dns.AAAA); ok {
+					respIP = a.AAAA
+					respTTL = a.Hdr.Ttl
+				} else {
+					log.Printf("Upstream response not an AAAA record; got %T", in.Answer[0])
+				}
+			}
+		}
+		if len(respIP) == 0 {
+			respIP = d.respAAAA
+		}
+		if respTTL <= 0 {
+			respTTL = defaultTTL
+		}
+
+		resp.Answer = append(resp.Answer, &dns.AAAA{
+			Hdr: dns.RR_Header{
+				Name:   q.Name,
+				Rrtype: dns.TypeAAAA,
+				Class:  dns.ClassINET,
+				Ttl:    respTTL,
+			},
+			AAAA: respIP.To16(),
+		})
+
+	case dns.TypeA:
+		if shouldForward(q.Name) {
+			in, err := dns.Exchange(req, fmt.Sprintf("%s:53", d.upstream))
+			if err != nil {
+				log.Printf("Error requesting from upstream DNS: %s", err)
+			} else {
+				if a, ok := in.Answer[0].(*dns.A); ok {
+					respIP = a.A
+					respTTL = a.Hdr.Ttl
+				} else {
+					log.Printf("Upstream response not an A record; got %T", in.Answer[0])
+				}
+			}
+		}
+		if len(respIP) == 0 {
+			respIP = d.respA
+		}
+		if respTTL <= 0 {
+			respTTL = defaultTTL
+		}
+
+		resp.Answer = append(resp.Answer, &dns.A{
+			Hdr: dns.RR_Header{
+				Name:   q.Name,
+				Rrtype: dns.TypeA,
+				Class:  dns.ClassINET,
+				Ttl:    respTTL,
+			},
+			A: respIP.To4(),
+		})
+
+	default:
+		resp.SetRcode(req, dns.RcodeRefused)
+	}
+
+	if err := w.WriteMsg(&resp); err != nil {
+		log.Printf("error responding to %q: %s", req, err)
+	}
+}
